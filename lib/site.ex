91436f1b1275fef643b8841fffbf7d21b22c5c11
Matt Savoia <matt@1-800-rad-dude.com>
2023-12-17 18:11:02 -0500

    Update Site.render_file, use Template

--- lib/site.ex
+++ lib/site.ex
@@ -1,13 +1,14 @@
 defmodule Blog.Site do
   use Phoenix.Component
   import Phoenix.HTML
+  alias Phoenix.Template
 
   @output_dir "./output"
   File.mkdir_p!(@output_dir)
 
   def build do
     posts = Blog.all_posts()
-    render_file("index.html", index(%{posts: posts}))
+    render_file("index.html", "index", %{posts: posts})
 
     for post <- posts do
       dir = Path.dirname(post.path)
@@ -16,14 +17,14 @@ defmodule Blog.Site do
         File.mkdir_p!(Path.join([@output_dir, dir]))
       end
 
-      render_file(post.path, post(%{post: post}))
+      render_file(post.path, "post", %{post: post})
     end
 
     :ok
   end
 
-  def render_file(path, rendered) do
-    safe = Phoenix.HTML.Safe.to_iodata(rendered)
+  def render_file(path, template, assign) do
+    safe = Template.render_to_iodata(__MODULE__, template, "html", assign)
     output = Path.join([@output_dir, path])
 
     File.write!(output, safe)
