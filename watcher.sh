089b7feb1df736e49c9df33eddf7acdb847182f0
Matt Savoia <matt@1-800-rad-dude.com>
2023-12-19 10:38:19 -0500

    Update watcher, ignore output dir

--- watcher.sh
+++ watcher.sh
@@ -1,3 +1,3 @@
-while inotifywait -q --recursive --event modify --exclude "build|data|.git|logfile|watcher.sh" .; do
+while inotifywait -q --recursive --event modify --exclude "build|output|.git|watcher.sh" .; do
     mix build
 done
