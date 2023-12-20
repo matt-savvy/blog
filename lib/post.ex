930d44383f8a18c2c99074145cfff2defa372d78
Matt Savoia <matt@1-800-rad-dude.com>
2023-12-20 09:48:49 -0500

    Refactor Post build, extract parse_filename

--- lib/post.ex
+++ lib/post.ex
@@ -21,11 +21,19 @@ defmodule Blog.Post do
   ]
 
   def build(filename, attrs, body) do
+    {path, date, id} = parse_filename(filename)
+    struct!(__MODULE__, [id: id, date: date, body: body, path: path] ++ Map.to_list(attrs))
+  end
+
+  defp parse_filename(filename) do
     path = Path.rootname(filename)
+
     [year, month_day_id] = path |> Path.split() |> Enum.take(-2)
-    path = path <> ".html"
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
-    struct!(__MODULE__, [id: id, date: date, body: body, path: path] ++ Map.to_list(attrs))
+
+    path = path <> ".html"
+
+    {path, date, id}
   end
 end
