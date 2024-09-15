086c76f5cd7e198790767f467c8b5ed6d15f1802
Matt Savoia <matt@1-800-rad-dude.com>
2024-09-15 13:02:53 -0400

    Update HTMLConverter, add unsafe render
    Allows us to embed youtube videos into the markdown.

--- lib/html_converter.ex
+++ lib/html_converter.ex
@@ -1,5 +1,9 @@
 defmodule Blog.HTMLConverter do
   def convert(_filepath, body, _atrs, _opts) do
-    MDEx.to_html(body, features: [syntax_highlight_theme: "nightfox"], extension: [table: true])
+    MDEx.to_html(body,
+      features: [syntax_highlight_theme: "nightfox"],
+      extension: [table: true, tagfilter: false],
+      render: [unsafe_: true]
+    )
   end
 end
