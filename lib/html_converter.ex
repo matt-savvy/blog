0ad8ec19389546ce0237bb448ec9825e93f3345e
Matt Savoia <matt@1-800-rad-dude.com>
2024-09-15 12:36:28 -0400

    Update HTMLConverter, add table extension

--- lib/html_converter.ex
+++ lib/html_converter.ex
@@ -1,5 +1,5 @@
 defmodule Blog.HTMLConverter do
   def convert(_filepath, body, _atrs, _opts) do
-    MDEx.to_html(body, features: [syntax_highlight_theme: "nightfox"])
+    MDEx.to_html(body, features: [syntax_highlight_theme: "nightfox"], extension: [table: true])
   end
 end
