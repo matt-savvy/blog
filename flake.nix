ee9af27ac20788ae9941f26a06df3672115510be
Matt Savoia <matt@1-800-rad-dude.com>
2025-05-05 13:59:16 -0400

    Add flake for devShell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+          ];
+          packages = with pkgs; [
+            beam.packages.erlang_27.elixir_1_18
+            beam.packages.erlang_27.elixir-ls
+            caddy
+            (writeShellScriptBin "file-server" ''
+              caddy file-server --listen :8000 --root ./output
+            '')
+          ];
+        };
+      }
+    );
+}
