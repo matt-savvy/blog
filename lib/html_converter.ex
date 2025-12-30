67b320cbc2d017d0af6df6fb0657617e4013f675
Matt Savoia <matt@1-800-rad-dude.com>
2025-12-29 22:35:43 -0500

    Update HTMLConverter, use to_html!

--- lib/html_converter.ex
+++ lib/html_converter.ex
@@ -1,12 +1,9 @@
 defmodule Blog.HTMLConverter do
   def convert(_filepath, body, _atrs, _opts) do
-    {:ok, html} =
-      MDEx.to_html(body,
-        syntax_highlight: [formatter: {:html_inline, theme: "nightfox"}],
-        extension: [table: true, tagfilter: false, footnotes: true],
-        render: [unsafe_: true]
-      )
-
-    html
+    MDEx.to_html!(body,
+      syntax_highlight: [formatter: {:html_inline, theme: "nightfox"}],
+      extension: [table: true, tagfilter: false, footnotes: true],
+      render: [unsafe_: true]
+    )
   end
 end
