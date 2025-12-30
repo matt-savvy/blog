3414de4922911a816125ff63000b313397354f72
Matt Savoia <matt@1-800-rad-dude.com>
2025-12-29 22:34:39 -0500

    Bump mdex to 0.10

--- lib/html_converter.ex
+++ lib/html_converter.ex
@@ -2,7 +2,7 @@ defmodule Blog.HTMLConverter do
   def convert(_filepath, body, _atrs, _opts) do
     {:ok, html} =
       MDEx.to_html(body,
-        features: [syntax_highlight_theme: "nightfox"],
+        syntax_highlight: [formatter: {:html_inline, theme: "nightfox"}],
         extension: [table: true, tagfilter: false, footnotes: true],
         render: [unsafe_: true]
       )
