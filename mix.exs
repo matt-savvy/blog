74ffa799237911d04d8db4316148fd69efaf09b3
Matt Savoia <matt@1-800-rad-dude.com>
2025-12-29 22:36:57 -0500

    Update minimum elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blog.MixProject do
     [
       app: :blog,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
